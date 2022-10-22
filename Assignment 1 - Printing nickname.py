80a23d4e9c204735a063daef16a0debe54e9fb7e
unknown <angelaescariocorpuz@gmail.com>
2022-10-22 13:17:24 +0800

    added code to print the letter L and minor revisions

--- Assignment 1 - Printing nickname.py
+++ Assignment 1 - Printing nickname.py
@@ -4,7 +4,9 @@
 from colorama import Fore, Back, Style 
 
 G = [[" " for row in range (5)] for column in range (5)]
-E = [[" " for row in range (5)] for column in range (4)]
+E = [[" " for row in range (5)] for column in range (5)]
+L = [[" " for row in range (5)] for column in range (5)]
+A = [[" " for row in range (5)] for column in range (5)]
 
 for g_row in range (5):
     for g_column in range (5):
@@ -12,14 +14,22 @@ for g_row in range (5):
             G [g_column][g_row]= "*"
 
 for e_row in range (5):
-    for e_column in range (4):
+    for e_column in range (5):
         if e_row == 0 or e_row == 2 or e_row == 4 or ((e_row == 1 or e_row ==3) and e_column == 0):
             E [e_column][e_row] = "*"
 
+for l_row in range (5):
+    for l_column in range (5):
+        if l_column == 0 or l_row == 4:
+            L [l_column][l_row] = "*"
+
 for row in range (5):
     for column in range (5):
-        print (Fore.BLUE + G [column][row], end = " ")
-    print (end = "  ")
-    for column in range (4):
+        print (G [column][row], end = " ")
+    print (end = "   ")
+    for column in range (5):
         print (E [column][row], end = " ")
+    print (end = "   ")
+    for column in range (5):
+        print (L [column][row], end = " ")
     print ()
